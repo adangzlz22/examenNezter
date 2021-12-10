725aef8f4cba37147b95a2c52e456fe37eef21f3
Manuel Gamez <manuel.gamez@ext-cemex.com>
2021-12-10 01:56:39 -0600

    Ng bootstrap

--- src/app/components/usuarios/usuarios.component.ts
+++ src/app/components/usuarios/usuarios.component.ts
@@ -1,7 +1,7 @@
 import { OnDestroy, ViewChild, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ApiService } from 'src/app/services/api.service';
-import { DataTableDirective } from 'angular-datatables';
+import { ApiService } from 'src/app/services/api.service';  
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-usuarios',
@@ -9,9 +9,10 @@ import { DataTableDirective } from 'angular-datatables';
   styleUrls: ['./usuarios.component.css']
 })
 export class UsuariosComponent implements OnInit {
-  
+  title = 'Create User';
+  closeResult:string = '';
   users = [];
-  constructor(private route: Router,private userService: ApiService) { }
+  constructor(private route: Router,private userService: ApiService,private modalService: NgbModal ) { }
 
   ngOnInit(): void {
     if (!this.userService.isLogged) {
@@ -24,6 +25,24 @@ export class UsuariosComponent implements OnInit {
     this.userService.post('Login/ObtenerUsuarios',objModel).subscribe( result => {
       this.users = JSON.parse(result['Model']);
     });
+  }
+
+  openModal(content:any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 
 }
